7047541de92dea91095718a8e2130e0a99026d89
TCB <49961674+bttger@users.noreply.github.com>
2019-07-09 22:23:52 +0200

    added implementation relevant constants

--- spotirip/const.py
+++ spotirip/const.py
@@ -3,10 +3,13 @@ CLIENT_ID = "aLotOfNumbersAndCharacters"
 CLIENT_SECRET = "aLotOfNumbersAndCharacters"
 REDIRECT_URL = "http://localhost/"
 
-MEAN_RESP_TIME = 150
-
 SOUNDCARD_ID = "{0.0.0.00000000}.{e4f0594c-5e6f-436d-ab0e-4ecaf7f8djf7d8}"
 
 # only for mp3 exports
 BITRATE = 320
 FRAMERATE = 44100
+
+# implementation relevant, because starting the recording has some latency
+FORERUN = 3  # in seconds
+DELTA = 2  # in seconds, shifting the recorded frame, different on every pc and internet connection TODO automatic
+MAX_SONG_LENGTH = 180  # in seconds, maximum length of recorded songs
